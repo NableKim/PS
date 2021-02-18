136c66df8fd8c87a3185b39cea9a632fe7fedf37
Nablekim <Nablekim94@gmail.com>
2021-02-18 22:16:43 +0900

    PG42746 "가장 큰 수" 문제풀이 업로드

--- VS_PROJ_FOR_PS/VS_PROJ_FOR_PS/Programmers/Level3/PG42746.cpp
+++ VS_PROJ_FOR_PS/VS_PROJ_FOR_PS/Programmers/Level3/PG42746.cpp
@@ -4,45 +4,32 @@
 #include <algorithm>
 using namespace std;
 
-string solution(vector<int> numbers);
-
-struct A {
-    int n1, n2;
-};
-
-bool cmp(A& a, A& b) {
-    if (a.n1 == b.n1)
-        return a.n2 < b.n2;
-    return a.n1 > b.n1;
-}
-
-int main() {
-    vector<int> numbers = { 0, 0, 0, 1 };
-    cout << solution(numbers);
-	return 0;
+bool cmp(string& s1, string& s2) {
+    if (s1 + s2 > s2 + s1)
+        return true;
+    return false;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
-    vector<A> v;
+
+    vector<string> v;
     
-    int nSize = numbers.size();
-    for (int i = 0; i < nSize; i++) {
-        int n = numbers[i];
-        if (n) {
-            while (n < 1000)
-                n = (n * 10) + (n % 10);
-        }
-        v.push_back({ n, numbers[i] });
-    }
+    for (int& n : numbers)
+        v.push_back(to_string(n));
 
     sort(v.begin(), v.end(), cmp);
 
-    if (v[0].n2 == 0)
+    if (v[0]=="0")
         return "0";
-
-    for (int i = 0; i < nSize; i++)
-        answer += to_string(v[i].n2);   
 
     return answer;
 }
+
+int main() {
+    vector<int> numbers = { 334, 33 };
+    cout << solution(numbers);
+	return 0;
+}
+
+
