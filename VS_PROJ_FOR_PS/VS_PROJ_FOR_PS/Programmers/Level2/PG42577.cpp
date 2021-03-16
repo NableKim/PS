b920e5b5384c7afa08af2f3ced8c4a4ac1bc9f3c
Nablekim <Nablekim94@gmail.com>
2021-03-16 22:46:53 +0900

    PG42577 '전화번호 목록' 문제풀이

--- VS_PROJ_FOR_PS/VS_PROJ_FOR_PS/Programmers/Level2/PG42577.cpp
+++ VS_PROJ_FOR_PS/VS_PROJ_FOR_PS/Programmers/Level2/PG42577.cpp
@@ -2,26 +2,29 @@
 #include <vector>
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
+bool cmp(string& a, string& b) {
+    return a.size() > b.size();
+}
+
 bool solution(vector<string> phone_book) {
-    // 해쉬 만들고
-    unordered_map<string, bool> um;
+    // 사전 순으로 정렬
+    sort(phone_book.begin(), phone_book.end(), cmp);
 
     // key에 value값을 true로 기록
-    for (string& s : phone_book)
-        um.insert(make_pair(s, true));
-    
-    int pSize = phone_book.size();
+    unordered_map<string, bool> um;
+
     // 첫번째 문자열부터 짤라내면서 짜른 문자열에 해당하는 value값이 트루면 false 리턴 끝
     for (string& s : phone_book) {
+        if (um.find(s) != um.end())    // 찾았다면
+            return false;
+
         int sSize = s.size();
-        for (int i = 0; i < pSize; i++) {
+        for (int i = 0; i < sSize; i++) {
             string s2 = s.substr(0, i + 1);
-            if (um.find(s2) != um.end()) {    // 찾았다면
-                if(s2 != s)
-                    return false;
-            }
+            um.insert(make_pair(s2, true));
         }
     }
     return true;
@@ -29,7 +32,7 @@ bool solution(vector<string> phone_book) {
 
 int main() {
 
-    cout << solution({ "123","456","789" });
+    cout << solution({ "119", "97674223", "1195524421" });
 
     return 0;
 }
