080339179304fc5b22c01160e0f0f89feb17ea42
Nablekim94 <Nablekim94@gmail.com>
2021-10-05 20:52:22 +0900

    백준 2941번 문제풀이

--- ProblemSolving/ProblemSolving/BJ2941_크로아티아알파벳.cpp
+++ ProblemSolving/ProblemSolving/BJ2941_크로아티아알파벳.cpp
@@ -9,7 +9,51 @@ int main() {
 	string input;
 	cin >> input;
 
-	int index;
+	int index = 0;
+	
+	int answer = 0;
+	int len = input.length();
+	while (index < len) {
+
+		if (index == len - 1) {
+			answer++;
+			break;
+		}
+
+		string tmp = "";
+		switch (input[index])
+		{
+		case 'c' :
+			if (input[index + 1] == '=' || input[index + 1] == '-')
+				index++;
+			break;
+		case 'd':
+			if (len - index >= 3
+				&& input[index + 1] == 'z'
+				&& input[index + 2] == '=')
+				index += 2;
+			else if (input[index + 1] == '-')
+				index++;
+			break;
+		case 'l':
+		case 'n':
+			if (input[index + 1] == 'j')
+				index++;
+			break;
+		case 's':
+		case 'z':
+			if (input[index + 1] == '=')
+				index++;
+			break;
+		default:
+			break;
+		}
+
+		index++;
+		answer++;
+	}
+
+	cout << answer;
 
 	return 0;
 }
