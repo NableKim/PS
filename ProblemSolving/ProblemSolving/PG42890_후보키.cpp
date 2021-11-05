5ccdc11f0e8f6b05cebf9acc97807c596ae5c201
nablekim94 <nablekim94@gmail.com>
2021-11-05 20:25:21 +0900

    프로그래머스 42890번 문제파일 생성(미해결)

---
+++ ProblemSolving/ProblemSolving/PG42890_후보키.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<string>> relation) {
+	int answer = 0;
+	return answer;
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//freopen("Text.txt", "r", stdin);
+
+
+	return 0;
+}
