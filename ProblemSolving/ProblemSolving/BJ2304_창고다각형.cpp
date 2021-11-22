f1cd3bdd331540a172628d4e1439f900d041e521
nablekim94 <nablekim94@gmail.com>
2021-11-22 13:29:41 +0900

    백준 2304번 문제풀이

--- ProblemSolving/ProblemSolving/BJ2304_창고다각형.cpp
+++ ProblemSolving/ProblemSolving/BJ2304_창고다각형.cpp
@@ -1,9 +1,16 @@
+/*
+한쪽 방향에서 시작해서 다음 값을 확인 후, 
+더 높은 기둥이 나오는지 or 낮은 기둥이 나오는지에 따라 값을 다르게 누적하는 방식
+*/
+
 #include <cstdio>
 using namespace std;
 
-int map[1001];
+int map[1002];
 
 int main() {
+
+	//freopen("Text.txt", "r", stdin);
 
 	int N;
 	scanf("%d", &N);
@@ -19,38 +26,27 @@ int main() {
 	}
 
 	int left = minIdx, right = maxIdx;
-	
+	int turn = map[left]>map[right]? 1: 0;
+
 	int sum = map[left];
 	if (left != right)
 		sum += map[right];
 
-	int turn = 0;
-	int preLeft = left++;
-	int preRight = right--;
-	while (left <= right) {
-
+	while (right-left>1) {
 		if (turn == 0) {
-			if (map[preLeft] >= map[left]) {
-				sum += map[preLeft];
-				left++;
-			}
-			else {
-				sum += map[left];
-				preLeft = left++;
-				turn ^= 1;
-			}
+			if (map[left] >= map[left + 1])
+				map[left + 1] = map[left];
+			left++;
+			sum += map[left];
 		}
 		else {
-			if (map[right] <= map[preRight]) {
-				sum += map[preRight];
-				right--;
-			}
-			else {
-				sum += map[right];
-				preRight = right--;
-				turn ^= 1;
-			}
+			if (map[right-1] <= map[right])
+				map[right-1] = map[right];
+			right--;
+			sum += map[right];
 		}
+
+		turn = map[left] > map[right] ? 1 : 0;
 	}
 
 	printf("%d", sum);
