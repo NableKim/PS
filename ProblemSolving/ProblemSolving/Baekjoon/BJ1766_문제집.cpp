f722276ed99a44ebfa2de90d456a2578c70d31aa
Nablekim94 <Nablekim94@gmail.com>
2021-12-16 23:42:30 +0900

    백준 1766번 문제풀이

--- ProblemSolving/ProblemSolving/Baekjoon/BJ1766_문제집.cpp
+++ ProblemSolving/ProblemSolving/Baekjoon/BJ1766_문제집.cpp
@@ -1,25 +1,24 @@
 #include <cstdio>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
 #define NODE_NUN 32000
 
-int D[NODE_NUN + 1];
-vector<int> E[NODE_NUN + 1];
-bool check[NODE_NUN + 1];
+struct P {
+	int degree, idx;
+};
 
-void explorer(int node) {
-	if (D[node] == 0) {
-		printf("%d ", node);
-		check[node] = true;
-		for (int k = 0; k < E[node].size(); k++) {
-			int nextNode = E[node][k];
-			D[nextNode]--;
-			explorer(nextNode);
-		}
-	}
+bool operator <(P a, P b) {
+	if (a.degree == b.degree)
+		return a.idx > b.idx;
+	return a.degree > b.degree;
 }
+
+int Degree[NODE_NUN + 1];
+bool Check[NODE_NUN + 1];
+vector<int> E[NODE_NUN + 1];
+priority_queue<P> pq;
 
 int main() {
 
@@ -31,17 +30,31 @@ int main() {
 	for (int i = 0, a, b; i < M; i++) {
 		scanf("%d %d", &a, &b);
 		E[a].push_back(b);	// 간선 정보 저장
-		D[b]++;	// b의 차수 증가
+		Degree[b]++;		// b의 차수 증가
 	}
 
 	for (int i = 1; i <= N; i++) {
-		if (E[i].size() >= 2)
-			sort(E[i].begin(), E[i].end());
+		pq.push({ Degree[i], i });
 	}
 
-	for (int i = 1; i <= N; i++) {
-		if(!check[i])
-			explorer(i);
+	int cnt = 0;
+	while (!pq.empty()) {
+		int node = pq.top().idx;
+		pq.pop();
+	
+		if (!Check[node]) {
+			Check[node] = true;
+			printf("%d ", node);
+			cnt++;
+
+			if (cnt == N) break;
+
+			for (int k = 0; k < E[node].size(); k++) {
+				int nextNode = E[node][k];
+				Degree[nextNode]--;
+				pq.push({ Degree[nextNode], nextNode });
+			}
+		}
 	}
 
 	return 0;
