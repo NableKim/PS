0f9259a0c4fbb45f8e9a6c0126c837f8e371ba63
Nablekim94 <Nablekim94@gmail.com>
2021-12-21 23:47:22 +0900

    백준 2661번 문제풀이(미해결)

--- ProblemSolving/ProblemSolving/Baekjoon/BJ2661_좋은수열.cpp
+++ ProblemSolving/ProblemSolving/Baekjoon/BJ2661_좋은수열.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N;
-string str = "";
+string str = "12";
 
 void dfs(int depth) {
 	if (depth == N) {
@@ -11,10 +11,22 @@ void dfs(int depth) {
 	}
 
 	bool check[4] = { false, };
-	if(str.size() >= 1)
-		check[str[str.size() - 1] - '0'] = true;
-	if(str.size() >= 2)
-		check[str[str.size() - 2] - '0'] = true;
+
+	// 바로 직전의 원소
+	int left = 0, right;
+	for (int len = depth/2; len >=1; len--) {
+		right = left + len;
+		bool flag = true;
+		for (int i = 0; i < len-1; i++) {
+			if (str[left + i] != str[right + i]) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag)
+			check[str[left + len - 1] - '0'] = true;
+		left = right;
+	}
 
 	for (int i = 1; i <= 3; i++) {
 		if (!check[i]) {
@@ -27,10 +39,13 @@ void dfs(int depth) {
 
 
 int main() {
-	
+
 	scanf("%d", &N);
 
-	dfs(0);
+	if (N < 2)
+		str.pop_back();
+	else
+		dfs(2);
 
 	printf("%s", str.c_str());
 
