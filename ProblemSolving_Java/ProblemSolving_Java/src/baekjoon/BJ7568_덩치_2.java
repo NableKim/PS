f0a2da75705233be78fb90c9615d7b82c956ab07
Nablekim94 <Nablekim94@gmail.com>
2021-12-22 23:52:58 +0900

    백준 7568번 문제풀이 with java bufferedreader

---
+++ ProblemSolving_Java/ProblemSolving_Java/src/baekjoon/BJ7568_덩치_2.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ7568_µ¢Ä¡_2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] person = new int[N][2];
+		
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			person[i][0] = Integer.parseInt(st.nextToken());
+			person[i][1] = Integer.parseInt(st.nextToken());			
+		}
+		
+		for(int i=0; i<N; i++) {			
+			int cnt = 1;
+			for(int j=0; j<N; j++) {
+				if(i==j) continue;
+				if(person[i][0] < person[j][0] && person[i][1] < person[j][1])
+					cnt++;
+			}
+			System.out.print(cnt + " ");
+		}
+	}
+}
---
+++ ProblemSolving_Java/Readme.md
@@ -0,0 +1,16 @@
+* Scanner vs BufferedReader
+BufferedReader는 한줄을 통째로 입력받는 방법으로 Scanner보다 입력 속도가 비교적 빠름
+
+```java
+
+public class BJ7568_덩치_2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄을 읽고 공백을 기준으로 토큰화
+		int a = Integer.parseInt(st.nextToken());	// 첫번째로 입력받은 정수
+		int b = Integer.parseInt(st.nextToken());	// 두번째로 입력받은 정수
+	}
+}
+```
