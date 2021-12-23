50bbc2d00933da2cca430942a954b3e7c3866ff5
Nablekim94 <Nablekim94@gmail.com>
2021-12-23 21:57:06 +0900

    백준 2750번 문제풀이 (BufferedWriter 사용)

--- ProblemSolving_Java/ProblemSolving_Java/src/baekjoon/BJ2750_수정렬하기.java
+++ ProblemSolving_Java/ProblemSolving_Java/src/baekjoon/BJ2750_수정렬하기.java
@@ -1,26 +1,42 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class BJ2750_수정렬하기 {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		for(int i=0; i<N; i++) {
-			int a = Integer.parseInt(br.readLine());
-			arr[i] = a;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N;
+		try {
+			N = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[N];
+			for(int i=0; i<N; i++) {
+				arr[i] = Integer.parseInt(br.readLine());
+			}
+			
+			Arrays.sort(arr); // 오름차순 정렬
+			
+			for(int i : arr)
+				bw.write(i + "\n");
+			
+			bw.flush();
+			bw.close();
+			br.close();
+			
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		Arrays.sort(arr); // 오름차순 정렬
-		
-		for(int i : arr)
-			System.out.println(i);
 		
 		
 	}
