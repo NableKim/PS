e32bbced7b86431455e2ac2f4e01d8fa800def5f
nablekim94 <nablekim94@gmail.com>
2021-12-29 23:20:28 +0900

    프로그래머스 64062번 문제풀이(dq로 주어진 배열에서 구간마다 최대값 구하기 이용)

--- ProblemSolving/ProblemSolving/Programmers/BJ64062_징검다리건너기.cpp
+++ ProblemSolving/ProblemSolving/Programmers/BJ64062_징검다리건너기.cpp
@@ -1,43 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 using namespace std;
+
+struct P {
+    int idx, val;
+};
 
 int solution(vector<int> stones, int k) {
     int answer = 200000000;
 
-    // 왼쪽에서 오른쪽 방향
-    int cnt = 0;
-    int pre = -1;
-    for (int i = 0; i < stones.size(); i++) {
-        if (stones[i] >= pre) {
-            cnt++;
-            if (cnt == k) {
-                int minVal = stones[i];
-                if(i>=k)
-                    minVal = stones[i-k] > stones[i]? stones[i]: stones[i - k];
-                answer = answer > minVal ? minVal : answer;
-            }
-                
-        }
-        else 
-            cnt = 1;
-        pre = stones[i];
-    }
+    // k개의 디딤돌 구간에서 최대값이 있을텐데 그 최대값들 중에 가장 작은 값이 정답임
+    deque<P> dq;
 
-    for (int i = stones.size()-1; i >= 0; i--) {
-        if (stones[i] >= pre) {
-            cnt++;
-            if (cnt == k) {
-                int minVal = stones[i];
-                if (i <= stones.size()-1-k)
-                    minVal = stones[i + k] > stones[i] ? stones[i] : stones[i + k];
-                answer = answer > minVal ? minVal : answer;
-            }
-        }
-        else
-            cnt = 1;
-        pre = stones[i];
+    for (int i = 0; i < stones.size(); i++) {
+
+        // 현재 살펴볼 디딤돌보다 k이상 떨어진 디딤돌 값은 제거
+        if (!dq.empty() && dq.front().idx <= i - k)
+            dq.pop_front();
+
+        // 현재 살펴볼 디딤돌의 값보다 작은 값들은 pop
+        while(!dq.empty() && dq.back().val <= stones[i])
+            dq.pop_back();
+        
+        dq.push_back({ i, stones[i] });
+        if (i >= k - 1)
+            answer = answer > dq.front().val ? dq.front().val : answer;
     }
 
     return answer;
@@ -46,10 +35,10 @@ int solution(vector<int> stones, int k) {
 
 int main() {
 
-    vector<int> stones = { 6,1,2,5,4,10 };
+    //vector<int> stones = { 6,1,2,5,4,10 };
     //vector<int> stones = { 2, 4, 5, 3, 2, 1, 4, 2, 5, 1 };
-    //vector<int> stones = { 2, 2};
-    int k = 4;
+    vector<int> stones = { 2, 2 };
+    int k = 2;
 
     cout << solution(stones, k);
 
