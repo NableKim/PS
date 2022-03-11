69526ef9952a8d28261665443f9bb5ce5b8bb0c2
nablekim94 <nablekim94@gmail.com>
2022-03-11 15:22:34 +0900

    간결한 코드로 리팩토링

--- ProblemSolving/ProblemSolving/Programmers/PG92335_k진수에서소수개수구하기.cpp
+++ ProblemSolving/ProblemSolving/Programmers/PG92335_k진수에서소수개수구하기.cpp
@@ -1,42 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<string> split(string str) {
-    
-    vector<string> v;
-    int pre = 0;
-    int idx = 0;
-    bool flag = false;
-    while (idx < str.length()) {
-        if (str[idx] == '0') {
-            if (!flag)  {
-                string tmp = str.substr(pre, idx - pre);
-                v.push_back(tmp);
-                flag = true;
-            }
-        }
-        else {
-            if (flag == true) {
-                flag = false;
-                pre = idx;
-            }
-        }
-
-        idx++;
-    }
-
-    if (!flag) {
-        string tmp = str.substr(pre, idx - pre);
-        v.push_back(tmp);
-    }
-
-    return v;
-}
-
 bool isPrime(long long n) {
-    if (n == 1) return false;
+    if (n < 2) return false;
 
     for (long long i = 2; i * i <= n; i++) {
         if (n % i == 0)
@@ -45,32 +14,36 @@ bool isPrime(long long n) {
     return true;
 }
 
-int solution(int n, int k) {
-    int answer = 0;
-
-    // k진법으로 표현하기
-
-    string str = "";
-    while (n >= k) {
-        int r = n % k;
-        n = n / k;
-        str = to_string(r) + str;
-    }
-    if (n != 0)
-        str = to_string(n) + str;
-
-    // 0을 기준으로 나누기
-    vector<string> v = split(str);
+int splitNCnt(string str) {
     
-    // 그 수가 소수인지 판정 후 소수 갯수 증가
-    for (int i = 0; i < v.size(); i++) {
-        if (isPrime(stoll(v[i])))
-            answer++;
+    str += '0'; // 끝에 0 하나 추가
+    int answer = 0;
+    long long value = str[0] - '0';
+    for (int i = 1; i < str.size(); i++) {
+        if (str[i] == '0') {
+            isPrime(value) ? answer++: answer;
+            value = 0;
+        }
+        else
+            value = value * 10 + (str[i] - '0');
     }
+    return answer;
+}
 
+int solution(int n, int k) {
+    // k진법으로 표현하기
+    string str = "";
+    while (n) {
+        str += ((n % k)+'0');
+        n /= k;
+    }
+    reverse(str.begin(), str.end());
+
+    // 0을 기준으로 나눠서 소수인지 검증
+    int answer = splitNCnt(str);
     return answer;
 }
 
 int main() {
-    cout << solution(21, 2);
+    cout << solution(11011011, 10);
 }
