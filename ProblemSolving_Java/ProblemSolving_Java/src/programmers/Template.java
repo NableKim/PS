1330454f3f81a15799179a2a156edcb107678671
nablekim94 <nablekim94@gmail.com>
2022-04-22 12:46:29 +0900

    프로그래머스 12973번 문제풀이(with java)

---
+++ ProblemSolving_Java/ProblemSolving_Java/src/programmers/Template.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class Template {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int answer = solution.solution();
+        System.out.println("answer = " + answer);
+
+//        int[] answers = solution.solution();
+//        for(int i=0; i<answers.length; i++)
+//            System.out.println(answers[i] + " ");
+    }
+
+    static class Solution {
+        public int solution() {
+
+            return 0;
+        }
+    }
+}
